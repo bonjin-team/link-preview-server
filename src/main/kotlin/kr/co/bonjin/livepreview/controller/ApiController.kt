639ddff74e5381652643a88a951d0acf1d3827ed
bonjin <bonjin.app@gmail.com>
2023-11-09 09:32:34 +0900

    feat: 쿠팡 대응 헤더 추가

--- src/main/kotlin/kr/co/bonjin/livepreview/controller/ApiController.kt
+++ src/main/kotlin/kr/co/bonjin/livepreview/controller/ApiController.kt
@@ -4,11 +4,13 @@ import kr.co.bonjin.livepreview.model.dto.PreviewRequestDto
 import kr.co.bonjin.livepreview.response.ApiDataResponse
 import kr.co.bonjin.livepreview.response.ApiResponse
 import kr.co.bonjin.livepreview.service.CrawlingService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 
+@CrossOrigin(origins = ["*"])
 @RestController
 class ApiController(
     private val crawlingService: CrawlingService
--- src/main/kotlin/kr/co/bonjin/livepreview/provider/JsoupProvider.kt
+++ src/main/kotlin/kr/co/bonjin/livepreview/provider/JsoupProvider.kt
@@ -5,11 +5,17 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.springframework.stereotype.Service
 import java.io.IOException
+import java.util.*
 
 @Service
 class JsoupProvider {
     fun get(url: String): PreviewModel? {
+        var headers = HashMap<String, String>()
+        headers["accept-encoding"] = "gzip, deflate, br"
+        headers["accept-language"] = "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7"
         val conn = Jsoup.connect(url)
+                        .timeout(2*1000)
+                        .headers(headers)
         try {
             val document = conn.get()
 
